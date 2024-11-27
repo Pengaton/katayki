6385acae8b04c7dd071536dba36d3983c918fcd3
Pengaton <lorddev707@gmail.com>
2024-11-27 20:45:45 +0700

    ⚡️ Added sound

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import soundFile from './assets/thank-you-katayki.mp3'
 import Img from './assets/katayki.jpg'
 
 import './App.css'
 
 function App() {
-  useEffect(() => {
-    const audio = new Audio(soundFile)
-    audio.loop = true
-    audio.play().catch((error) => {
-      console.error('Autoplay failed:', error)
-    })
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [audio] = useState(new Audio(soundFile))
 
+  // Play or pause the audio when the user clicks the button
+  const toggleAudio = () => {
+    if (isPlaying) {
+      audio.pause()
+    } else {
+      audio.loop = true
+      audio.play().catch((error) => {
+        console.error('Autoplay failed:', error)
+      })
+    }
+    setIsPlaying(!isPlaying)
+  }
+
+  useEffect(() => {
+    // Clean up audio when component unmounts
     return () => {
       audio.pause()
       audio.currentTime = 0
     }
-  }, [])
+  }, [audio])
 
   return (
-    <>
-      <div className="flex justify-center gap-4 w-full pt-4">
-        <p className="text-xl text-white">Buy here:</p>{' '}
-        <a href="https://pump.fun/coin/Fx3SkomgYuRkgCJJdhESp4FfcrYmBcWeS2He8L3spump" target="_blank" className="text-xl text-[#00FF00]">
+    <div className="w-full">
+      <div className="flex flex-col sm:flex-row justify-center gap-4 w-full pt-4 px-4 sm:px-8">
+        <p className="text-xl text-white text-center sm:text-left">Buy here:</p>{' '}
+        <a href="https://pump.fun/coin/Fx3SkomgYuRkgCJJdhESp4FfcrYmBcWeS2He8L3spump" target="_blank" rel="noopener noreferrer" className="text-xl text-[#00FF00] break-words sm:text-left">
           https://pump.fun/coin/Fx3SkomgYuRkgCJJdhESp4FfcrYmBcWeS2He8L3spump
         </a>
       </div>
-      <div className="flex justify-center items-center w-full h-screen">
-        <img src={Img} alt="$Katayki" className="w-[50%] lg:w-[30%] animate-scaleLoop" />
+
+      <div className="flex justify-center items-center w-full mt-2 mb-8">
+        <button onClick={toggleAudio} className="bg-[#00FF00] text-black px-6 py-2 rounded-full mb-4 text-xl sm:text-2xl">
+          {isPlaying ? 'Stop $Katayki' : 'Listen $Katayki'}
+        </button>
       </div>
-    </>
+
+      <div className="flex justify-center items-center w-full h-[70%] sm:h-[90%] mb-4">
+        <img src={Img} alt="$Katayki" className="w-[70%] sm:w-[50%] lg:w-[30%] animate-scaleLoop" />
+      </div>
+    </div>
   )
 }
 
